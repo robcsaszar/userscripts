37f765c1b43afb6dacedf3c08b8edfbb39128750
Rob Csaszar <rob.csaszar@gmail.com>
2022-08-29 17:57:33 +0300

    add Arena.js

---
+++ Truce/Arena.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Truce.gg: Show plunderable gold & experience
+// @namespace   https://github.com/robcsaszar
+// @version     1.2
+// @description Calculate plunderable player gold & experience
+// @author      Yukio
+// @attribution Brock Adams (https://github.com/BrockA)
+// @match       https://truce.gg/arena
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=truce.gg
+// @require     https://raw.githubusercontent.com/robcsaszar/Userscripts/Helpers/waitForKeyElements.js
+// @updateURL   https://raw.githubusercontent.com/robcsaszar/Userscripts/Truce/Arena.meta.js
+// @downloadURL https://raw.githubusercontent.com/robcsaszar/Userscripts/Truce/Arena.js
+// @supportURL  https://github.com/robcsaszar/Userscripts/issues
+// @grant       GM_addStyle
+// @grant       GM.getValue
+// @run-at      document-end
+// ==/UserScript==
